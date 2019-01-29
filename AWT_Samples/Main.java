85d4cbc8974d55a34bf295470c2ec47fd89881b3
Sayantan Das <sayantandasbhp@gmail.com>
2019-01-30 00:23:20 +0530

    AWT Library

---
+++ AWT_Samples/Main.java
@@ -0,0 +1,9 @@
+package com.Demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	MyWindow myWindow = new MyWindow("Successfull Attempt");
+	myWindow.setVisible(true);
+    }
+}
